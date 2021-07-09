9eb9dabdaca65a8642be85be3615e56d21c028c8
Sujoy Dev <sujoydev99@gmail.com>
2021-07-09 17:47:35 +0530

    extra dirs

--- run.py
+++ run.py
@@ -29,11 +29,17 @@ if(prefixLength<4):
 # folder loop
 for i in range(0,counter):
      print("processing folder no. " + str(i+1))
-     os.makedirs(destinationRoot+"/Folder_"+str(i+1)+"/input/normal")
-     os.makedirs(destinationRoot+"/Folder_"+str(i+1)+"/input/projection")
+     os.makedirs(destinationRoot+"/Frame_"+str(i+1)+"/Input/normal")
+     os.makedirs(destinationRoot+"/Frame_"+str(i+1)+"/Input/projection")
+     os.makedirs(destinationRoot+"/Frame_"+str(i+1)+"/Project")
+     os.makedirs(destinationRoot+"/Frame_"+str(i+1)+"/Result")
+     os.makedirs(destinationRoot+"/Frame_"+str(i+1)+"/Temp")
     #  image loop
      for j in range (0, increment):
          print(arr[j*counter+i])
-         shutil.copy(sourceRoot+"/"+arr[j*counter+i], destinationRoot+"/Folder_"+str(i+1)+"/input/normal/"+(str(j+1).zfill(prefixLength))+"."+arr[j*counter+i].split(".")[1])
-         shutil.copy(sourceRoot+"/"+arr[j*counter+i], destinationRoot+"/Folder_"+str(i+1)+"/input/projection/"+(str(j+1).zfill(prefixLength))+"."+arr[j*counter+i].split(".")[1])
+         shutil.copy(sourceRoot+"/"+arr[j*counter+i], destinationRoot+"/Frame_"+str(i+1)+"/Input/normal/"+(str(j+1).zfill(prefixLength))+"."+arr[j*counter+i].split(".")[1])
+         shutil.copy(sourceRoot+"/"+arr[j*counter+i], destinationRoot+"/Frame_"+str(i+1)+"/Input/projection/"+(str(j+1).zfill(prefixLength))+"."+arr[j*counter+i].split(".")[1])
+         shutil.copy(sourceRoot+"/"+arr[j*counter+i], destinationRoot+"/Frame_"+str(i+1)+"/Project/"+(str(j+1).zfill(prefixLength))+"."+arr[j*counter+i].split(".")[1])
+         shutil.copy(sourceRoot+"/"+arr[j*counter+i], destinationRoot+"/Frame_"+str(i+1)+"/Result/"+(str(j+1).zfill(prefixLength))+"."+arr[j*counter+i].split(".")[1])
+         shutil.copy(sourceRoot+"/"+arr[j*counter+i], destinationRoot+"/Frame_"+str(i+1)+"/Temp/"+(str(j+1).zfill(prefixLength))+"."+arr[j*counter+i].split(".")[1])
          
