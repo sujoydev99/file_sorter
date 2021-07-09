3e9976140505a9015421d956f90e06fa96e154b2
Sujoy Dev <sujoydev99@gmail.com>
2021-07-09 09:26:14 +0530

    renaming

--- run.py
+++ run.py
@@ -18,6 +18,14 @@ arr=os.listdir(sourceRoot)
 arr=sorted(arr)
 counter = int(len(arr)/int(increment))
 
+prefixLength = 0
+temp = len(arr)
+while (temp > 0):
+  temp = temp//10
+  prefixLength = prefixLength + 1
+if(prefixLength<4):
+    prefixLength = 4
+
 # folder loop
 for i in range(0,counter):
      print("processing folder no. " + str(i+1))
@@ -25,6 +33,7 @@ for i in range(0,counter):
      os.makedirs(destinationRoot+"/Folder_"+str(i+1)+"/input/projection")
     #  image loop
      for j in range (0, increment):
-         shutil.copy(sourceRoot+"/"+arr[j*counter+i], destinationRoot+"/Folder_"+str(i+1)+"/input/normal/"+arr[j*counter+i])
-         shutil.copy(sourceRoot+"/"+arr[j*counter+i], destinationRoot+"/Folder_"+str(i+1)+"/input/projection/"+arr[j*counter+i])
+         print(arr[j*counter+i])
+         shutil.copy(sourceRoot+"/"+arr[j*counter+i], destinationRoot+"/Folder_"+str(i+1)+"/input/normal/"+(str(j+1).zfill(prefixLength))+"."+arr[j*counter+i].split(".")[1])
+         shutil.copy(sourceRoot+"/"+arr[j*counter+i], destinationRoot+"/Folder_"+str(i+1)+"/input/projection/"+(str(j+1).zfill(prefixLength))+"."+arr[j*counter+i].split(".")[1])
          
